4e427478102fe0a151a8ac33a4a0c94980bd6937
raohassansoc <rao.hassan@soc-solution.com>
2024-01-08 20:54:30 +0500

    first commit

---
+++ apps/bridge/components/stargate/network.ts
@@ -0,0 +1,53 @@
+import { mainnet, arbitrum, optimism, polygon, fantom, avalanche, bsc } from 'wagmi/chains'
+import { ariettaTestNet } from './costomChaintestnet'
+import { ariettaMainNet } from './costomChain'
+// Chain Id is defined by Stargate
+const stargateNetowrk = [
+  {
+    chainId: 92522,
+    name: 'Arietta Main Net',
+    chain: ariettaMainNet,
+  },
+  {
+    chainId: 26388,
+    name: 'Arietta Testnet',
+    chain: ariettaTestNet,
+  },
+  {
+    chainId: 101,
+    name: 'Ethereum',
+    chain: mainnet,
+  },
+  {
+    name: 'BNB Chain',
+    chainId: 102,
+    chain: bsc,
+  },
+  {
+    chainId: 109,
+    name: 'Matic',
+    chain: polygon,
+  },
+  {
+    chainId: 106,
+    name: 'Avalanche',
+    chain: avalanche,
+  },
+  {
+    chainId: 112,
+    name: 'Fantom',
+    chain: fantom,
+  },
+  {
+    chainId: 110,
+    name: 'Arbitrum',
+    chain: arbitrum,
+  },
+  {
+    chainId: 111,
+    name: 'Optimism',
+    chain: optimism,
+  },
+]
+
+export const findChainByStargateId = (chainId: number) => stargateNetowrk.find((s) => s.chainId === chainId)
